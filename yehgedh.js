c2b977cd3359ab555fb640dfc2055a9dfe5b5d6d
jamessssssss2 <jamesraystark2@proton.me>
2024-11-26 17:13:38 +1100

    Update yehgedh.js

--- yehgedh.js
+++ yehgedh.js
@@ -1,25 +1,25 @@
 // Get the modal 
-var popup = document.getElementById("popup"); 
+//var popup = document.getElementById("popup"); 
  
 // Get the button that opens the modal 
-var btn = document.getElementById("openPopup"); 
+//var btn = document.getElementById("openPopup"); 
  
 // Get the <span> element that closes the modal 
-var span = document.getElementsByClassName("close")[0]; 
- 
+//var span = document.getElementsByClassName("close")[0]; 
+
 // When the user clicks the button, open the modal  
-btn.onclick = function() { 
-    popup.style.display = "block"; 
-} 
+//btn.onclick = function() { 
+  //  popup.style.display = "block"; 
+//} 
  
 // When the user clicks on <span> (x), close the modal 
-span.onclick = function() { 
-    popup.style.display = "none"; 
-} 
+//span.onclick = function() { 
+  //  popup.style.display = "none"; 
+//} 
  
 // When the user clicks anywhere outside of the modal, close it 
-window.onclick = function(event) { 
-    if (event.target == popup) { 
-        popup.style.display = "none"; 
-    } 
-} 
+//window.onclick = function(event) { 
+ //   if (event.target == popup) { 
+       // popup.style.display = "none"; 
+  //  } 
+//} 
