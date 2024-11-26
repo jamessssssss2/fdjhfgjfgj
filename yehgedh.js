baf3eee3135ffe844c13aef6d918c27ce312f855
jamessssssss2 <jamesraystark2@proton.me>
2024-11-26 17:31:47 +1100

    Update yehgedh.js

--- yehgedh.js
+++ yehgedh.js
@@ -1,1 +1,12 @@
-
+// Get the modal 
+var popup = document.getElementById("popup"); 
+ 
+// Get the <span> element that closes the modal 
+var span = document.getElementsByClassName("close")[0]; 
+ 
+ 
+// When the user clicks on <span> (x), close the modal 
+span.onclick = function() { 
+    popup.style.display = "none"; 
+} 
+ 
